5d6671907b8193e95b5625c7ae242f3842cbe6e0
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-17 20:54:26 +0000

    Declaração das variáveis para armazenar os dados

---
+++ NOVATO - Super Trunfo/desafio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("DESAFIO SUPER TRUNFO - NOVATO!\n");
+
+    // Declaração das variáveis para armazenar os dados das Cartas do Jogo
+    char estado1, estado2;
+    char codigoCidade1[5], codigoCidade2[5];
+    char nomeCidade1[50], nomeCidade2[50];
+    int populacaoCidade1, populacaoCidade2;
+    float areaCidade1, areaCidade2;
+    float pibCidade1, pibCidade2;
+    int pontosTuristicosCidade1, pontosTuristicosCidade2;
+
+    //
+
+
+
+
+
+
+
+
+}
