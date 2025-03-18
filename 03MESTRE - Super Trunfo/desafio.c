0338a07c1b99919dc5e14b9ac80b1c1a530b5497
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-18 01:15:43 +0000

    Realizando alterações nas declarações das variáveis

--- 03MESTRE - Super Trunfo/desafio.c
+++ 03MESTRE - Super Trunfo/desafio.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("DESAFIO SUPER TRUNFO - AVENTUREIRO!\n");
+    printf("DESAFIO SUPER TRUNFO - MESTRE!\n");
 
     // Declaração das variáveis para armazenar os dados das Cartas do Jogo
 
     char estado1, estado2;
     char codigoCidade1[5], codigoCidade2[5];
     char nomeCidade1[50], nomeCidade2[50];
-    int populacaoCidade1, populacaoCidade2;
+    unsigned long int populacaoCidade1, populacaoCidade2;
     float areaCidade1, areaCidade2;
     float pibCidade1, pibCidade2;
     int pontosTuristicosCidade1, pontosTuristicosCidade2;
     float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
     float pibPerCapitaCidade1, pibPerCapitaCidade2;
+    float superPoderCidade1, superPoderCidade2;
 
     // Leitura dos dados da Carta 1
 
